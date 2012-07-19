c0cd7dcd379bd3fd4e956869eee5a270d5702a22
nico <mister.wunderlich@gmail.com>
2012-07-19 14:21:27 +0200

    performanceschrauberei
    only remove multiples of primes

--- Problem 010/sum2.py
+++ Problem 010/sum2.py
@@ -1,4 +1,3 @@
-import math
 import time
 
 t0 = time.clock()
@@ -7,18 +6,20 @@ last_nbr = 2000000
 
 is_prime = [True] * (last_nbr+1)
 
-
-for i in range(2,len(is_prime)):
-    for k in range(i,(last_nbr/i)+1):
-        is_prime[i*k]=False
-
 is_prime[0]=False
 is_prime[1]=False
-is_prime[2]=True
+
+for i in range(2,len(is_prime)):
+    if is_prime[i]==True:
+        for k in range(i,(last_nbr/i)+1):
+            is_prime[i*k]=False
+
+
+
 
 #print(filter(lambda x: x[1]==True, enumerate(is_prime)))
-
 
 print(sum([i[0] for i in enumerate(is_prime) if i[1]==True]))
 
 print time.clock()-t0
+

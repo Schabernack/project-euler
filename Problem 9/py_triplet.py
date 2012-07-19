ceb1b8b945608bac3ac2097acedbe5fd437d1f70
nico <mister.wunderlich@gmail.com>
2012-07-19 12:15:15 +0200

    small performance improvement

--- Problem 9/py_triplet.py
+++ Problem 9/py_triplet.py
@@ -3,7 +3,7 @@ import time
 t0 = time.clock()
 
 for c in reversed(range(999)):
-    for b in range(1, c):
+    for b in range(1, 1000-c):
         a = 1000-b-c
         if 0<=a<b<c and a**2+b**2==c**2:
                 print a*b*c
